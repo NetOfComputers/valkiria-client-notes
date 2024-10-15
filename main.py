78fe9c6f9c4d773faf474d5a6934cdf84c2f9037
Jaime <73396010+JmJDrWrk@users.noreply.github.com>
2024-10-15 12:24:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 '''This file has the unique mision of let me testing download,start,stopetc methods in new controller'''
 
 import time
+print('Starting fake server')
+while True:
+  time.sleep(5)
+  print('Client sent: a message')
+  time.sleep(3)
+  print('GoAgain!')
